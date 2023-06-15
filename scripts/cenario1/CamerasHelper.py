0313a489263d73d7cea2bdf2f57793e5886234d2
Pedro <pedro.diehl@ayga.com.br>
2023-06-15 01:30:42 -0300

    updates to enhance code readability

--- scripts/cenario1/CamerasHelper.py
+++ scripts/cenario1/CamerasHelper.py
@@ -14,28 +14,14 @@ class Camera1Helper:
    def __init__(self):
       pass
 
-   def receive_cam1_message(self, message: roslibpy.Message) -> None:
-      image_np = self.ros_message_to_cv2_image(message)
-      cv2.imshow('CAM 1', image_np)
-      cv2.waitKey(1)
-
-      return
-
-   def process_cam1_image(self, image: np.ndarray):
-      return
+   def manipulate_cam1_image(self, image: np.ndarray):
+      pass
 
 
 class Camera2Helper:
    def __init__(self):
       pass
 
-   def receive_cam2_message(self, message: roslibpy.Message) -> None:
-      image_np = self.ros_message_to_cv2_image(message)
-      cv2.imshow('CAM 2', image_np)
-      cv2.waitKey(1)
-
-      return
-
-   def process_cam2_image(self, image: np.ndarray):
-      return
+   def manipulate_cam2_image(self, image: np.ndarray):
+      pass
 
--- scripts/cenario1/cenario1_solution.py
+++ scripts/cenario1/cenario1_solution.py
@@ -22,9 +22,13 @@ class Solver1(Camera1Helper, Camera2Helper):
       self.port = port
       self.listeners: dict = {}
 
-   def setup_cam_views(self) -> None:
-      self.subscribe_to_topic(cam_number=1, callback=self.receive_cam1_message)
-      self.subscribe_to_topic(cam_number=2, callback=self.receive_cam2_message)
+   def instantiate_client(self):
+      self.client: roslibpy.Ros = roslibpy.Ros(host=self.host, port=self.port)
+      return
+
+   def setup_cameras_views(self) -> None:
+      self.subscribe_to_topic(cam_number=1, callback=self.process_camera1_message)
+      self.subscribe_to_topic(cam_number=2, callback=self.process_camera2_message)
 
       return
 
@@ -39,27 +43,27 @@ class Solver1(Camera1Helper, Camera2Helper):
 
       return
 
-   def receive_cam1_message(self, message: roslibpy.Message) -> None:
-      image_np = self.ros_message_to_cv2_image(message)
+   def process_camera1_message(self, message: roslibpy.Message) -> None:
+      image_np = self.ros_message_to_image(message)
 
-      self.process_cam1_image(image_np)
+      self.manipulate_cam1_image(image_np)
 
       cv2.imshow('CAM 1', image_np)
       cv2.waitKey(1)
 
       return
 
-   def receive_cam2_message(self, message: roslibpy.Message) -> None:
-      image_np = self.ros_message_to_cv2_image(message)
+   def process_camera2_message(self, message: roslibpy.Message) -> None:
+      image_np = self.ros_message_to_image(message)
 
-      self.process_cam2_image(image_np)
+      self.manipulate_cam2_image(image_np)
 
       cv2.imshow('CAM 2', image_np)
       cv2.waitKey(1)
 
       return
 
-   def ros_message_to_cv2_image(self, message: roslibpy.Message) -> np.ndarray:
+   def ros_message_to_image(self, message: roslibpy.Message) -> np.ndarray:
       base64_bytes = message['data'].encode('ascii')
 
       image_bytes = base64.b64decode(base64_bytes)
@@ -68,17 +72,14 @@ class Solver1(Camera1Helper, Camera2Helper):
 
       return cv2.imdecode(np_array, cv2.IMREAD_COLOR)
 
-   def instantiate_client(self):
-      self.client: roslibpy.Ros = roslibpy.Ros(host=self.host, port=self.port)
-      return
-
    def run(self):
       self.client.run_forever()
 
    def execute(self):
       self.instantiate_client()
-      self.setup_cam_views()
+      self.setup_cameras_views()
       self.run()
+
 
 if __name__ == '__main__':
    ROS_HOST: str = os.getenv('ROS_HOST') 
