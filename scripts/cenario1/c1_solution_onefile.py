680567ed5a3e6b72e9bdb0a157fa9393bd7cf33d
Pedro <pedro.diehl@ayga.com.br>
2023-09-08 19:25:19 -0300

    working one file, robot still doesnt respond as expected

--- scripts/cenario1/c1_solution_onefile.py
+++ scripts/cenario1/c1_solution_onefile.py
@@ -25,14 +25,14 @@ talker = roslibpy.Topic(client, '/game/joy', 'sensor_msgs/Joy')
 def main(image: np.ndarray):
    cv2.imshow('Original', image)
 
-   blurred = cv2.GaussianBlur(image, (5, 5), 0)
-   gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-
-   difference, max_difference = calculate_difference(gray)
+   identified_path_image = identify_path(image)
+   difference, max_difference = calculate_difference(identified_path_image)
 
    mapped_difference = difference / max_difference
    draw_arrow(mapped_difference, image)
    cv2.waitKey(1)
+
+   print(f'Difference: {difference} | Max Difference: {max_difference} | Mapped Difference: {mapped_difference}')
 
    joystick_message = roslibpy.Message({
       'axes': [mapped_difference, 0.2],
@@ -42,7 +42,42 @@ def main(image: np.ndarray):
    talker.publish(joystick_message)
 
    time.sleep(1)
+
    return
+
+
+def identify_path(image: np.ndarray):
+   # Apply gaussian blur to reduce noise
+   blurred = cv2.GaussianBlur(image, (5, 5), 0)
+   #cv2.imshow('Blurred Image', blurred)
+
+   # Gray scale the image
+   gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+   #cv2.imshow('Gray Image', gray)
+
+   # Threshold the grayscale image to create a binary image
+   _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+
+   # Find contours in the binary image
+   contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+   # Create a mask for white contours on a black background
+   identified_path_image = np.zeros_like(blurred)
+
+   for contour in contours:
+      # Calculate the area of each contour
+      area = cv2.contourArea(contour)
+
+      # Filter contours based on area (adjust the threshold as needed)
+      if area > 1000:
+         # Draw the selected contour on the result image
+         cv2.drawContours(identified_path_image, [contour], -1, (0, 255, 0), thickness=cv2.FILLED)
+
+   # Display the result
+   #cv2.imshow('Identified Path', identified_path_image)
+
+   return identified_path_image
+
 
 def draw_arrow(mapped_difference: float, image: np.ndarray):
    # Draw a arrow scaled by mapped_difference
@@ -56,6 +91,7 @@ def draw_arrow(mapped_difference: float, image: np.ndarray):
 
    return
 
+
 def calculate_difference(image: np.ndarray):
    # Split the identified path image into left and right sides and crop the top
    height, width, _ = image.shape
@@ -67,14 +103,15 @@ def calculate_difference(image: np.ndarray):
    right_white_pixels = np.sum(right_side == 255)
 
    # Show the left and right sides
-   cv2.imshow('Left Side', left_side)
-   cv2.imshow('Right Side', right_side)
+   #cv2.imshow('Left Side', left_side)
+   #cv2.imshow('Right Side', right_side)
 
    # Return the difference between the number of white pixels in each side
    difference = left_white_pixels - right_white_pixels
    max_difference = left_side.shape[0] * left_side.shape[1]
 
    return (difference, max_difference)
+
 
 def receive_image(message):
    base64_bytes = message['data'].encode('utf-8')
